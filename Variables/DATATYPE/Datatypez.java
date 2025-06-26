7a8d2cb7a789feaa430420adf08439e1358ed6f2
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-06-26 22:38:20 +0530

    Add files via upload

---
+++ Variables/DATATYPE/Datatypez.java
@@ -0,0 +1,18 @@
+//there are  2 type of datatypes 1)primitive = int , float , char , boolean.    2)
+
+
+public class Datatypez {
+    public static void main(String[] args) {
+         int num1 = 9;
+         byte by = 127;
+         short sh = 558;
+         long l = 3535353;
+
+         float f = 5.8f;
+         double d = 5.6;
+
+         char c =  'k';
+
+         boolean b = true;
+    }
+}
---
+++ Variables/Vari2.java
@@ -0,0 +1,12 @@
+
+
+public class Vari2 {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 4
+        System.out.println(num1);
+    }
+}
+
+
+// = is assignment operator
