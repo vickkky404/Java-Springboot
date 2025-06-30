448ba8b4799a912c1654273faeb8b8fea2a8d89f
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-06-30 22:00:19 +0530

    Add files via upload

---
+++ Whilelp.java
@@ -0,0 +1,11 @@
+public class Whilelp {
+    public static void main(String[] args){
+        int i = 1;
+
+        while(i<=10){
+            System.out.println("This is the testing of while loop");
+            i++;
+        }
+    }
+    
+}
