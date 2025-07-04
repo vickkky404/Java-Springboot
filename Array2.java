3d292704389a1c5afd8521cbb5502e6a4318fc2a
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-04 21:23:37 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,24 @@
+public class Array2 {
+    public static void main(String[] args) {
+
+        // currently all the values in the arrays are zero
+        int nums[] = new int[3];
+
+        // assigning the values in the array from 0 to any other values..
+        nums[0] = 4;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 8;
+
+
+        // printing all the values present in the array using the loops concept..
+
+        for(int i = 0 ; i<=3 ; i++){
+            System.out.println(nums[i]);
+        }
+
+
+
+
+    }
+}
