46564cef44ff9b94af39ec4b04fee34cda000774
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-04 21:23:22 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,10 @@
+public class Array1 {
+    public static void main(String[] args){
+        int nums[] = {3,7,2,4};
+
+        // changing the value from address 1 i.e 7 to 6
+        nums[1] = 6;
+        System.out.println(nums[1]);
+
+    }
+}
