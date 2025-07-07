ffb1dce7941d4cc118acf60dd110fdf211d2a36b
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-07 22:01:47 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,15 @@
+public class String1 {
+    public static void main(String[] args) {
+        String name = "Navin";
+        System.out.println(name);
+
+
+        // String object creation..
+        String str = new String("Naveen");
+        System.out.println(str);
+
+        
+
+    }
+    
+}
---
+++ String2.java
@@ -0,0 +1,18 @@
+public class String2 {
+    public static void main(String[] args) {
+        String name = new String("Nalinikant");
+        String str = "Naveen";
+
+        // String methods
+
+        // concanate string....
+        System.out.println("Hello " + name + " and " + str);
+
+        System.out.println(name.concat(str));
+
+        String Myname = "Nalinikant";
+        Myname = Myname + " Bastia";
+        System.out.println("hello " + Myname);
+    }
+    
+}
