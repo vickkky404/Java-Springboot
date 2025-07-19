90bf65e5ddcf0f2b58fa8210d95f6c829a63a095
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-19 22:23:08 +0530

    Add files via upload

---
+++ ConstructorInjection/src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+public class Address{
+    private  String city;
+    private   String state;
+
+    public Address(String city , String name){
+        this.city = city;
+        this.state = state;
+    }
+    public String getCity(){
+        return city;
+    }
+
+    public String getState(){
+        return state;
+    }
+    @Override
+    public String toString() {
+        return "Address [City: " + city + ", State: " + state + "]";
+    }
+
+
+}
+public class Employee{
+    private int id;
+    private  String name;
+    private  String
+
+
+}
