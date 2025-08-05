4984d41f8cfed8fb94c7af680c17920eb8f7be85
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-08-05 23:23:32 +0530

    Add files via upload

---
+++ Cutm/src/main/java/com/cutm/cutm/CutmApplication.java
@@ -0,0 +1,15 @@
+package com.cutm.cutm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CutmApplication.class, args);
+        System.out.println("Hello world");
+
+    }
+
+}
---
+++ Cutm/src/test/java/com/cutm/cutm/CutmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutm.cutm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
