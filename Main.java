b441b3cbd7987270c8bbcd62d86488bcb4c9e313
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-14 20:16:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating objects of Car class
+        Car car1 = new Car("Red", "Toyota", 2020);
+        Car car2 = new Car("Blue", "Honda", 2018);
+
+        // Calling methods on objects
+        car1.displayDetails();
+        car2.displayDetails();
+    }
+}
