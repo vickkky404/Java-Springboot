0ab30f08833b3f90d719efeebc4ba687f72cd7ea
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-22 22:25:44 +0530

    Add files via upload

---
+++ SpringBoot/Scopes/ScopesSpring/src/test/java/com/scopes/scopesspring/ScopesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scopes.scopesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
