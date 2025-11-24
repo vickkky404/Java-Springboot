9a595db3df96549151394a6b0f6693336f23b489
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-11-24 22:13:31 +0530

    Add files via upload

---
+++ 01/SpringNew/src/main/java/com/nalinikant/springbootdemo/SpringNewApplication.java
@@ -0,0 +1,15 @@
+package com.nalinikant.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNewApplication.class, args);
+        System.out.println("Hello World!");
+
+    }
+
+}
---
+++ 01/SpringNew/src/test/java/com/nalinikant/springbootdemo/SpringNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalinikant.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
